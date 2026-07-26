0512f7ca718b383f57fc153076273c145fe42c4f
Resul Tankilic <resultankilic.business@gmail.com>
2026-07-26 21:53:36 +0300

    chore: add security headers, host google fonts locally, and implement SEO optimizations

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/outfit/300.css'
+import '@fontsource/outfit/400.css'
+import '@fontsource/outfit/500.css'
+import '@fontsource/outfit/600.css'
+import '@fontsource/outfit/700.css'
+import '@fontsource/outfit/800.css'
+import '@fontsource/outfit/900.css'
 import './index.css'
 import App from './App.tsx'
 
